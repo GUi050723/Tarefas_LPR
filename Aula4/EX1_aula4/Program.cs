2111aae464b8dfffcf794d8e4e6ac8a4df5fb0d1
Guilherme Souza <152984208+GUi050723@users.noreply.github.com>
2025-03-21 09:29:18 -0300

    Add files via upload

---
+++ Aula4/EX1_aula4/Program.cs
@@ -0,0 +1,8 @@
+﻿int numero;
+Console.Write("Digite um numero");
+numero=int.Parse(Console.ReadLine());
+if (numero %2==0){
+Console.WriteLine("Seu número é par!!");
+}else{
+    Console.WriteLine("Seu número é impar!!");
+}
