9c5ddb789bea3c12536da4b90aff9d3ec7259636
Guilherme Souza <152984208+GUi050723@users.noreply.github.com>
2025-06-13 09:34:01 -0300

    Add files via upload

---
+++ Aula8/ex2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+public struct Produto
+{
+    public string nome;
+    public int codigo;
+    public float preco;
+    public int qtd;
+}
+class aula8
+{
+    static void Main()
+    {
+        Produto[] produtos = new Produto[3];
+        for (int i = 0; i < produtos.Length; i++)
+        {
+            Console.WriteLine($"Digite os dados do produto {i + 1}:");
+
+            Console.Write("Nome: ");
+            produtos[i].Nome = Console.ReadLine();
+
+            Console.Write("Código: ");
+            produtos[i].Codigo = int.Parse(Console.ReadLine());
+
+            Console.Write("Preço: ");
+            produtos[i].Preco = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantidade: ");
+            produtos[i].Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+        }
+
+        double valorTotal = 0;
+
+        for (int i = 0; i < produtos.Length; i++)
+        {
+            valorTotal += produtos[i].Preco * produtos[i].Quantidade;
+        }
+
+        Console.WriteLine($"Valor total em estoque: R$ {valorTotal:F2}");
+    }
+}
+    
