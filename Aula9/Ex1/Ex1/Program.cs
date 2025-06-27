098932a989fe8a6a4c87c7b28fdd9d39f7f969b5
Guilherme Souza <152984208+GUi050723@users.noreply.github.com>
2025-06-27 09:31:25 -0300

    Add files via upload

---
+++ Aula9/Ex1/Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+List<string> nomes = new();
+System.Console.WriteLine("Digite os nomes");
+nomes.Add(Console.ReadLine());
+while (true)
+{
+string entrada = Console.ReadLine();
+if (entrada.ToLower() == "fim")// to lower transforma tudo em minuscula
+nomes.Add(entrada);
+}
+List<string> nomesOrdenados = nomes.OrderBy(nome => nome.Length).ToList(); // coloca em ordem de caracter!!!
+foreach (string nome in nomesOrdenados)
+{
+Console.WriteLine(nome);
+}
